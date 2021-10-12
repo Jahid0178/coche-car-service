b26f3283aae1b541efc69ab15342bbd5e4ea6192
Jahid <jahidulhaque767@gmail.com>
2021-10-13 03:24:05 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Services from "./components/Services/Services";
+import NotFound from "./components/NotFound/NotFound";
+import NavBar from "./components/Shared/NavBar/NavBar";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <h2>This is app</h2>
+    <div>
+      <Router>
+        <NavBar></NavBar>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/services">
+            <Services></Services>
+          </Route>
+          <Route path="/contact">
+            <Contact></Contact>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/Shared/NavBar/NavBar.js
+++ src/components/Shared/NavBar/NavBar.js
@@ -1,10 +1,30 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 
 const NavBar = () => {
   return (
     <div>
-      <h2>NavBar</h2>
+      <Navbar bg="dark" variant="dark">
+        <Container fluid>
+          <Navbar.Brand href="#home">Coche</Navbar.Brand>
+          <Nav className="ms-auto">
+            <NavLink className="nav-link" to="/home">
+              Home
+            </NavLink>
+            <NavLink className="nav-link" to="/about">
+              About
+            </NavLink>
+            <NavLink className="nav-link" to="/services">
+              Services
+            </NavLink>
+            <NavLink className="nav-link" to="/contact">
+              Contact
+            </NavLink>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 };
