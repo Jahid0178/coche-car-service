4810144fc99854fa79b9759c95eaa17fff7f8c05
Jahid <jahidulhaque767@gmail.com>
2021-10-13 03:00:55 +0600

    file upload

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCSUYwtLSLrStKCR4wxZ9iZm4vklMIgMwk",
+  authDomain: "coche-car-service.firebaseapp.com",
+  projectId: "coche-car-service",
+  storageBucket: "coche-car-service.appspot.com",
+  messagingSenderId: "986722869985",
+  appId: "1:986722869985:web:f70e4d01c0da9e666fcda0",
+};
+
+export default firebaseConfig;
